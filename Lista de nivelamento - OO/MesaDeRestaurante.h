c12b50bd413747bba548363174d37acad78b90da
SAndradeTC <satc1624@gmail.com>
2020-09-15 22:22:58 -0300

    codigo - classe 2

---
+++ Lista de nivelamento - OO/MesaDeRestaurante.h
@@ -0,0 +1,25 @@
+#ifndef MESA_RESTAURANTE_H
+#define MESA_RESTAURANTE_H
+
+#define MAX_MESAS 10
+
+#include "Pedido.h"
+#include <list>
+
+class MesaDeRestaurante
+{
+public:
+    MesaDeRestaurante();
+    MesaDeRestaurante(  int numeroMesa, Pedido *ptrPedido );
+
+    void adicionaAoPedido( Pedido *ptrPedido );
+    void zeraPedidos();
+    double calculaTotal();
+
+    bool setNumeroMesa( int numeroMesa );
+private:
+    std::list<Pedido*> pedidos;
+    int numeroMesa;
+};
+
+#endif
